d412f9f16e78109fc842c97c5cb00c005315052c
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2023-08-19 21:12:15 -0600

    FINALIZADO

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,44 +2,72 @@
 del primer corte de la asignatura "ESTRUCTURAS DE DATOS"
 
 Menú
-1. La nota más alta
-2. La nota más baja
+1. Presentar el nombre y nota más alta
+2. Presentar el nombre y nota más baja
 3. El promedio de las notas
-4. Debe permitir editar cualquier nota
+4. Debe permitir editar cualquier nombre o nota
 5. Mostrar las notas ordenadas de menor a mayor
 */
 
 #include <iostream>
+#define MAX 5
 using namespace std;
 
+typedef struct{
+    char nombreCompleto[100];
+    float notas;
+} estudiantes;
+
+estudiantes notasDefinitivas[MAX];
+
+void menuPrincipal(int cant, double x);
+void nombreONota(int cant, double x);
+void mostrarEstudiantes(int cant);
+void regresarAlMenuPrincipal(int cant, double x);
+
 int main(){
-    int cant, temp, opc;
-    double x, notaAlta = -100, notaBaja = 100, promedio;
+    int cant;
+    double x;
 
-    cout << "Cuantas notas del primer corte de la asignatura ESTRUCTURA DE DATOS desea ingresar:" << endl;
+    cout << "Cuantos estudiantes del primer corte de la asignatura ESTRUCTURA DE DATOS deseas ingresar:" << endl;
     cin >> cant;
-    double notas[cant];
 
     system("cls");
 
-    for (int i = 0; i < cant; i++){
-        cout << "Ingresa la nota #" << i+1 << ":" << endl;
-        cin >> notas[i];
-        x = x + notas[i];
+    if (cant < MAX && cant > 0){
+        for (int i = 0; i < cant; i++){
+        cout << "Ingrese el nombre completo del estudiante # " << i+1 << ":" << endl;
+        scanf(" %[^\n]", notasDefinitivas[i].nombreCompleto);
+        cout << "Ingrese su nota del 1er Corte: " << endl;
+        cin >> notasDefinitivas[i].notas;
+        x = x + notasDefinitivas[i].notas;
+        cout << endl;
+        }
+        system("pause");
+        system("cls");
+        menuPrincipal(cant, x);
     }
+    else{
+        cout << "Ha escrito un valor fuera del rango permitido." << endl;
+        system("pause");
+        system("cls");
+        main();
+    }
+    return 0;
+}
 
-    system("pause");
-    system("cls");
+void menuPrincipal(int cant, double x){
+    int temp, ejercicio;
+    double notaAlta = -100, notaBaja = 100, promedio;
 
-    int ejercicio;
     cout << endl;
     cout << "----------ESTRUCTURA DE DATOS----------" << endl;
     cout << "      Notas definitivas - I Corte      " << endl;
     cout << endl;
-    cout << "  1. La nota mas alta." << endl;
-    cout << "  2. La nota mas baja." << endl;
+    cout << "  1. Mostrar la nota mas alta." << endl;
+    cout << "  2. Mostrar la nota mas baja." << endl;
     cout << "  3. El promedio de notas." << endl;
-    cout << "  4. Editar notas." << endl;
+    cout << "  4. Editar notas o nombre de estudiante." << endl;
     cout << "  5. Mostrar las notas ordenadas de menor a mayor." << endl;
     cout << endl;
     cin >> ejercicio;
@@ -47,64 +75,78 @@ int main(){
     switch (ejercicio)
     {
     case 1: //nota mas alta
+        int pos;
         for (int i = 0; i < cant; i++){
-            if (notas[i] > notaAlta){
-                notaAlta = notas[i];
+            if (notasDefinitivas[i].notas > notaAlta){
+                notaAlta = notasDefinitivas[i].notas;
+            }
+        }
+        for (int i = 0; i < cant; i++){
+            if (notaAlta == notasDefinitivas[i].notas){
+                pos = i;
             }
         }
         cout << endl;
-        cout << "La nota mas alta ingresada es " << notaAlta << endl;
+        cout << "----NOTA MAS ALTA----" << endl;
+        cout << "Estudiante: " << notasDefinitivas[pos].nombreCompleto << endl;
+        cout << "Nota: " << notaAlta << endl;
+        system("pause");
+        system("cls");
+        regresarAlMenuPrincipal(cant, x);
         break;
     
     case 2: //nota mas baja
         for (int i = 0; i < cant; i++){
-            if (notas[i] < notaBaja){
-                notaBaja = notas[i];
+            if (notasDefinitivas[i].notas < notaBaja){
+                notaBaja = notasDefinitivas[i].notas;
+            }
+        }
+        for (int i = 0; i < cant; i++){
+            if (notaBaja == notasDefinitivas[i].notas){
+                pos = i;
             }
         }
         cout << endl;
-        cout << "La nota mas baja ingresada es " << notaBaja << endl;
+        cout << "----NOTA MAS BAJA----" << endl;
+        cout << "Estudiante: " << notasDefinitivas[pos].nombreCompleto << endl;
+        cout << "Nota: " << notaBaja << endl;
+        system("pause");
+        system("cls");
+        regresarAlMenuPrincipal(cant, x);
         break;
 
     case 3: //promedio
         promedio = x/cant;
         cout << endl;
         cout << "El promedio de notas es " << promedio << endl;
+        system("pause");
+        system("cls");
+        regresarAlMenuPrincipal(cant, x);
         break;
     
     case 4: //editar el arreglo
         system("cls");
-        cout << "Ingrese el # de nota que deseas modificar: " << endl;
-        cin >> opc;
-        if (opc <= cant){
-            cout << "Nuevo valor en nota #" << opc << ":" << endl;
-            cin >> notas[opc-1];
-        }
-        else{
-            cout << "Esa posicion no existe." << endl;
-        }
-        cout << endl;
-        cout << "Lista modificada: " << endl;
-        for (int i = 0; i < cant; i++){
-            cout << notas[i] << endl;
-        }
+        nombreONota(cant, x);
         break;
 
     case 5:
-        system("clear"); 
+        system("cls"); 
         cout << "Notas ordenadas: " << endl;
         for (int i = 0; i < cant; i++){
             for (int j = i+1; j < cant; j++){
-                if (notas[i] > notas[j]){
-                    temp = notas[j];
-                    notas[j] = notas[i];
-                    notas[i] = temp;
+                if (notasDefinitivas[i].notas > notasDefinitivas[j].notas){
+                    temp = notasDefinitivas[j].notas;
+                    notasDefinitivas[j].notas = notasDefinitivas[i].notas;
+                    notasDefinitivas[i].notas = temp;
                 }
             }
         }
         for (int i = 0; i < cant; i++){
-            cout << notas[i] << endl;
+            cout << notasDefinitivas[i].notas << endl;
         }
+        system("pause");
+        system("cls");
+        regresarAlMenuPrincipal(cant, x);
         break;
 
     default:
@@ -114,6 +156,91 @@ int main(){
         main();
         break;
     }
+}
 
-    return 0;
+void nombreONota(int cant, double x){
+    int opc, posicion;
+    cout << "SELECCIONE QUE DESEA MODIFICAR:" << endl;
+    cout << endl;
+    cout << "  1. Nombre de estudiante." << endl;
+    cout << "  2. Nota de estudiante." << endl;
+    cout << endl;
+    cin >> opc;
+
+    switch(opc){
+        case 1: 
+            cout << "Ingrese el # del estudiante que deseas modificar: " << endl;
+            cin >> posicion;
+            if (posicion <= cant){
+                cout << "Edite el nombre del estudiante #" << posicion << ":" << endl;
+                scanf(" %[^\n]", notasDefinitivas[posicion-1].nombreCompleto);
+            }
+            else{
+                cout << "Ese numero no existe." << endl;
+            }
+            system("cls");
+            cout << "Lista modificada: " << endl;
+            cout << endl;
+            mostrarEstudiantes(cant);
+            system("pause");
+            system("cls");
+            regresarAlMenuPrincipal(cant, x);
+            break;
+
+        case 2:
+            cout << "Ingrese el # del estudiante que deseas modificar: " << endl;
+            cin >> posicion;
+            if (posicion <= cant){
+                cout << "Escribe la nueva nota del estudiante #" << posicion << ":" << endl;
+                cin >> notasDefinitivas[posicion-1].notas;
+            }
+            else{
+                cout << "Ese numero no existe." << endl;
+            }
+            system("cls");
+            cout << "Lista modificada: " << endl;
+            cout << endl;
+            mostrarEstudiantes(cant);
+            system("pause");
+            system("cls");
+            regresarAlMenuPrincipal(cant, x);
+            break;
+
+        default: 
+            cout << "Has ingresado un numero invalido. Intente otra vez." << endl;
+            system("pause");
+            system("cls");
+            nombreONota(cant, x);
+            break;
+    }
+}
+
+void mostrarEstudiantes(int cant){
+    for (int i = 0; i < cant; i++){
+        cout << "Estudiante: " << notasDefinitivas[i].nombreCompleto << endl;
+        cout << "Nota: " << notasDefinitivas[i].notas << endl;
+        cout << endl;
+    }
+}
+
+void regresarAlMenuPrincipal(int cant, double x){
+    int xd;
+    cout << "QUIERES REGRESAR AL MENU PRINCIPAL?" << endl;
+    cout << endl;
+    cout << "  1. SI" << endl;
+    cout << "  O presione cualquier numero para salir del programa" << endl;
+    cout << endl;
+    cin >> xd;
+
+    switch(xd){
+        case 1: 
+            system("cls");
+            menuPrincipal(cant, x);
+            break;
+
+        default: 
+            system("cls");
+            cout << "HA SALIDO EXITOSAMENTE" << endl;
+            break;
+    }
 }
